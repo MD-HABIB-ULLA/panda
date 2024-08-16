5e19bc05edd5962756c0605c8c30614f310c1610
habib ulla <habibulla1278@gmail.com>
2024-08-16 23:48:53 +0600

    vercel link added

--- src/Hooks/useAxiosPpablic.jsx
+++ src/Hooks/useAxiosPpablic.jsx
@@ -2,8 +2,8 @@ import axios from "axios";
 
 const axiosPublic = axios.create({
   // baseURL: "https://gym-server-pi.vercel.app",
-  // baseURL: "https://panda-searver.vercel.app",
-  baseURL: "http://localhost:3000",
+  baseURL: "https://panda-searver.vercel.app",
+  // baseURL: "http://localhost:3000",
 });
 
 const useAxiosPablic = () => {
